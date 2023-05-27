95deaac7530a011fdc642089ee724504de233828
feXXon <RobertGuzaerov123@yandex.ru>
2023-05-27 10:41:56 +0300

    Добавьте файлы проекта.

---
+++ PrakLaba5Zad4/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrakLaba5Zad4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double X = 0.5; 
+            int N = 5;
+            double result = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                double term = Math.Pow(-1, i - 1) * Math.Pow(X, i) / i;
+                result += term;
+            }
+
+            result += X;
+
+            txt1.Text = ($"{1 + X}: {result}");
+        }
+    }
+}
---
+++ PrakLaba5Zad5/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrakLaba5Zad5
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
